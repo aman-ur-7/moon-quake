a5400b5cb0d94d69d4ac225eb571d6ced52d7af1
Aman <amanjamad01@gmail.com>
2023-09-29 02:45:25 +0530

    second commit

---
+++ vite-project/src/GloabalStyle.jsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+    *{
+        padding: 0px;
+        margin: 0px;
+        box-sizing: border-box;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+`;
---
+++ vite-project/src/LoadingPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadingPage = () => {
+  return <div className="spinner">LoadingPage</div>;
+};
+
+export default LoadingPage;
